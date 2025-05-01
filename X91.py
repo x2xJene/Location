a27897283787aa42ed0f5a0c634bc42968b99539
X91 <168773525+x2xJene@users.noreply.github.com>
2025-05-01 21:45:10 +0100

    Add files via upload

---
+++ X91.py
@@ -0,0 +1,46 @@
+from pystyle import *
+
+import subprocess
+import  time
+
+def locals_server():
+    print ("local server started ....")
+    subprocess.Popen(["php","-S","127.0.0.1:8080"],stdout=subprocess.DEVNULL)
+    time.sleep(2)
+
+def x9_sever():
+    print ("local server started ....")
+    subprocess.Popen(["php","-S","127.0.0.1:8080"],stdout=subprocess.DEVNULL)
+    time.sleep(2)
+
+    print ("CloudFlare Tunnel started ....")
+    subprocess.Popen(["cf.exe","tunnel","-url","127.0.0.1:8080"],stdout=subprocess.DEVNULL)
+    time.sleep(10)
+
+def x91() :
+    print('''⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⠘⢰⡃⠔⠩⠤⠦⠤⢀⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⠄⢒⠒⠺⠆⠈⠀⠀⢐⣂⠤⠄⡀⠯⠕⣒⣒⡀⠀
+⠀⠀⢐⡡⠔⠁⠆⠀⠀⠀⠀⠀⢀⠠⠙⢆⠀⠈⢁⠋⠥⣀⣀
+⠈⠉⠀⣰⠀⠀⠀⠀⡀⠀⢰⣆⢠⠠⢡⡀⢂⣗⣖⢝⡎⠉⠀
+⢠⡴⠛⡇⠀⠐⠀⡄⣡⢇⠸⢸⢸⡇⠂⡝⠌⢷⢫⢮⡜⡀⠀
+⠀⠀⢰⣜⠘⡀⢡⠰⠳⣎⢂⣟⡎⠘⣬⡕⣈⣼⠢⠹⡟⠇⠀
+⠀⠠⢋⢿⢳⢼⣄⣆⣦⣱⣿⣿⣿⣷⠬⣿⣿⣿⣿⠑⠵⠀⠀
+⠀⠀⠀⡜⢩⣯⢝⡀⠁⠀⠙⠛⠛⠃⠀⠈⠛⠛⡿⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣿⠢⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣀⡇⠀⠑⠀⠀⠀⠀⠐⢄⠄⢀⡼⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⣿⣷⣤⣀⠈⠲⡤⣀⣀⠀⡰⠋⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣶⣤⣙⣷⣅⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⣾⣿⣿⣿⣿⣻⢿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀
+⠀⡠⠟⠁⠙⠟⠛⠛⢿⣿⣾⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀
+''')
+    print(Colors.green + "[+] By X91 : " )
+    choice = input( Colors.red+"[+] Use cloudFlare Tunnel? (Y/N) [Default : Y] :" ) .strip().lower()
+    if choice in ["n","no"]:
+        locals_server()
+    else:
+        x9_sever()
+if __name__ == "__main__":
+    x91()
+    while True:
+        time.sleep(1)
